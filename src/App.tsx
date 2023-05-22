852014ec7b2a0256d536b384bc84e7d7b0bb3f47
leonraz58 <leonraz58@gmail.com>
2023-05-22 17:11:30 +0300

    samuray 1.07

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,35 @@ import './App.css';
 function App() {
   return (
     <div>
-      <div>first divka</div>
-      <div className="App">
-        Second divka
-        Hello, samurai! Let's go!
-        <ul>
-          <li>css</li>
-          <li>html</li>
-          <li>js</li>
-          <li>react</li>
-        </ul>
-      </div>
+      <Header />
+      <Technologies />
     </div>
   );
+}
+
+const Header = () => {
+  return (
+    <div>
+      <a>Home</a>
+      <a>News feed</a>
+      <a>Messenger</a>
+    </div>
+  )
+}
+
+const Technologies = () => {
+  return (
+    <div>
+    Second divka
+    Hello, samurai! Let's go!
+    <ul>
+      <li>css</li>
+      <li>html</li>
+      <li>js</li>
+      <li>react</li>
+    </ul>
+  </div>
+  )
 }
 
 export default App;
