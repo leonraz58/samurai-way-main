b21e13e15ecac4df388710511a5539d823b85dd8
leonraz58 <leonraz58@gmail.com>
2023-06-06 17:28:30 +0300

    base layout / 11 completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,47 @@
-//import React from 'react';
+import React from 'react';
 import './App.css';
-import Header from './Header';
-import Technologies from './Technologies';
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <Technologies />
-    </div>
-  );
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src="https://dynamic.placementindia.com/recruiter_comp_logo/309213.png" alt="logo"/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a href="">Profile</a>
+                </div>
+                <div>
+                    <a href="">Messages</a>
+                </div>
+                <div>
+                    <a href="">News</a>
+                </div>
+                <div>
+                    <a href="">Music</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img width={200}
+                         src="https://tricksbystg.org/wp-content/uploads/2017/10/rainbow_background-wallpaper-2560x1600.jpg"
+                         alt=""/>
+                </div>
+                <div>
+                    avatar+description
+                </div>
+                <div>my posts
+                    <div>newpost</div>
+                    <div>
+                        <div>post1</div>
+                        <div>post2</div>
+                        <div>post3</div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
