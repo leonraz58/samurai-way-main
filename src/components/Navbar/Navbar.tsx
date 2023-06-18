c432ea81047a32b0d4a933e64bd948b0e16813b8
leonraz58 <leonraz58@gmail.com>
2023-06-18 18:04:32 +0300

    19 Route

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -9,10 +9,10 @@ export const Navbar = ()=>{
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <a>Profile</a>
+                <a href="/profile">Profile</a>
             </div>
             <div className={s.item}>
-                <a>Messages</a>
+                <a href="/dialogs">Messages</a>
             </div>
             <div className={s.item}>
                 <a href="">News</a>
