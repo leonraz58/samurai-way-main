f11492f403e651eaeafc300176f16ec0bf9a458e
leonraz58 <leonraz58@gmail.com>
2023-06-21 12:49:23 +0300

    22 Route exact

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,16 +1,23 @@
 import s from './Dialogs.module.css'
-
+import {NavLink} from "react-router-dom";
 
 type DialogsPropsType = {}
 export const Dialogs = (Props: DialogsPropsType) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.dialog+' '+s.active}>Димыч</div>
-                <div className={s.dialog}>Вася</div>
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to="/dialogs/1">Димыч</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/2">Андрей</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/3">Вася</NavLink>
+                </div>
             </div>
             <div className={s.messages}>
-            <div className={s.message}>123</div>
+                <div className={s.message}>123</div>
                 <div className={s.message}>312</div>
             </div>
         </div>
