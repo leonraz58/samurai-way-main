82e0a440ddff9894842dd4926c7700b73d4d5e6f
leonraz58 <leonraz58@gmail.com>
2023-07-18 15:51:11 +0300

    31 addPost button

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef} from "react";
 import s from './MyPosts.module.css'
 import {Post} from "./Post/Post";
 import {postType} from "../../../redux/state";
@@ -11,15 +11,22 @@ type MyPostsPropsType = {
 }
 export const MyPosts = (props:MyPostsPropsType) => {
 
-
+    let newPostElement = useRef<HTMLTextAreaElement>(null)
     let postsElements = props.posts.map(el => <Post message={el.message} likesCount={el.likesCount}/>)
+
+    const addPost = () => {
+        if (newPostElement.current !== null) {
+            alert(newPostElement.current.value)
+        }
+
+    }
 
     return (
         <div className={s.postsBlock}>
             <h3>my posts</h3>
             <div>
-                <div><textarea></textarea></div>
-                <div><button>addpost</button></div>
+                <div><textarea ref={newPostElement}></textarea></div>
+                <div><button onClick={addPost}>addpost</button></div>
             </div>
             <div className={s.posts}>
                 {/*<Post message={posts[0].message} likesCount={posts[0].likesCount}/>*/}
