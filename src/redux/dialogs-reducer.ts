caf75df7db6f3e313b72f26105ed5e3ca4fe8a12
leonraz58 <leonraz58@gmail.com>
2023-08-14 16:10:37 +0300

    41 reducer refactoring

--- src/redux/dialogs-reducer.ts
+++ src/redux/dialogs-reducer.ts
@@ -2,16 +2,32 @@ import {ActionsTypes, dialogsPageType, dialogsType, postType} from "./state";
 
 export const dialogsReducer = (state: dialogsPageType, action: ActionsTypes) => {
 
-    if (action.type === "UPDATE-NEW-MESSAGE-BODY") {
-        state.newMessageBody = action.body
-    } else {
-        if (action.type === "SEND-MESSAGE") {
+    switch (action.type) {
+        case "UPDATE-NEW-MESSAGE-BODY":
+            state.newMessageBody = action.body
+            return state
+        case "SEND-MESSAGE":
             let body = state.newMessageBody
             state.newMessageBody = ''
             state.messages.push({id: 6, message: body})
-        }
-        }
-
-
-    return state
+            return state
+        default:
+            return state
+    }
 }
+
+export const updateNewMessageBodyAC = (body: string) => {
+    return {
+        type: "UPDATE-NEW-MESSAGE-BODY",
+        body: body
+    } as const
+}
+
+export const sendMessageAC = () => {
+    return {
+        type: "SEND-MESSAGE"
+    } as const
+}
+
+type UpdateNewMessageBodyActionType = ReturnType<typeof updateNewMessageBodyAC>
+type SendMessageActionType = ReturnType<typeof sendMessageAC>
--- src/redux/profile-reducer.ts
+++ src/redux/profile-reducer.ts
@@ -1,21 +1,20 @@
 import {ActionsTypes, postType, profilePageType} from "./state";
 
 export const profileReducer = (state: profilePageType, action: ActionsTypes) => {
-    if (action.type === "ADD-POST") {
-        let newPost:postType = {
-            id: 4,
-            message: state.newPostText,
-            likesCount: 0
-        }
-        state.posts.push(newPost)
-        state.newPostText=''
-    } else {
-        if (action.type === "UPDATE-NEW-POST-TEXT") {
+    switch (action.type) {
+        case "ADD-POST":
+            let newPost:postType = {
+                id: 4,
+                message: state.newPostText,
+                likesCount: 0
+            }
+            state.posts.push(newPost)
+            state.newPostText=''
+            return state
+        case "UPDATE-NEW-POST-TEXT":
             state.newPostText = action.newText
-        } else {
-
-        }
+            return state
+        default:
+            return state
     }
-
-    return state
 }
